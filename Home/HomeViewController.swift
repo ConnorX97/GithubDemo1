0f5626e0cdfd6b8893997c8fbc58d7cfe196cc43
we <wer>
2022-04-16 17:14:52 +0900

    Initial Commit

---
+++ Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  GithubDemo1
+//
+//  Created by Sherzod Fayziev on 2022/04/16.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print ("Wellcome to Storyboard")
+      
+    }
+
+}
